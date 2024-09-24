35380ffa016fbc7f9cf01b106f5b6478966f6959
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-09-24 22:39:23 +0530

    added changes to main file

--- src/main/java/com/example/user_service/UserServiceApplication.java
+++ src/main/java/com/example/user_service/UserServiceApplication.java
@@ -3,9 +3,9 @@ package com.example.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
-
+@EnableJpaRepositories(basePackages = "com.example.user_service.repository")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
